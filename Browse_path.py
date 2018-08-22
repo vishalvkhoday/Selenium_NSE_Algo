404a1a24c65c121498ddc2eed974ef0c2bcb1491
vishalvkhoday <vishalvkhoday@gmail.com>
2018-08-22 17:09:17 +0530

    all Resportsfiles loaded

--- Browse_path.py
+++
@@ -1,9 +0,0 @@
-import Tkinter
-import tkMessageBox
-import tkFileDialog
-from Tkinter import Button
-from cgitb import text
-from distutils import command
-
-
-print "vishal"
--- Py_Join.py
+++
@@ -1,6 +0,0 @@
-
-i = 1
-while (i < 100000 ) :
-    i = i+1
-    print i
-    
