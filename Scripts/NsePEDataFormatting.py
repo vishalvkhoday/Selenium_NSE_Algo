bb5b8d6fe77407da18d1fb145c40f1dba0fa75d1
vishalvkhoday <vishalvkhoday@gmail.com>
2025-03-16 14:08:54 +0530

    Changes as on 16 March 2025

--- Scripts/NsePEDataFormatting.py
+++ Scripts/NsePEDataFormatting.py
@@ -6,24 +6,31 @@ LineVal = []
 ISIN = 0
 NsePEVal = open("C:\\Users\\Vishal\\OneDrive\\Desktop\\NSE_PE2.txt","r")
 for line in NsePEVal:
-    line = line.replace("\n",",")
-    if line.find(":Securities Information")>=0 :
-        line = line.replace(":Securities Information","")
-        ll= ll+ line
+    line = line.strip()
+    if len(line) <= 2:
+        continue
+    if ISIN == 0:
+        ll= ll+ line+","
+        ISIN = 1
+
+    if line.find("Securities Information")>=0 :
+        line = line.replace("Securities Information","")
+        ll= ll+ line.strip()
 
     if line.find("Basic Industry") >=0 :
-        line = line.replace("Basic Industry","")
-        rec= ll+ line +"\n"
+        line = line.replace("Basic Industry ",",")
+        rec= ll+ line.strip() +"\n"
         ll = ""
+        ISIN = 0
         f = open("C:\\Users\\Vishal\\OneDrive\\Desktop\\NSE_PE2_V1.txt","a")
         f.write(rec)
         f.close()
     
     if line.find("Adjusted P/E")>=0 :
-        line = line.replace("Adjusted P/E","")
+        line = line.replace("Adjusted P/E",",")
         ll= ll+ line
     if line.find("Symbol P/E")>=0 :
-        line = line.replace("Symbol P/E","")
+        line = line.replace("Symbol P/E",",")
         ll= ll+ line
 
 print(rec)
--- Scripts/Upstock_Nifty.py
+++ Scripts/Upstock_Nifty.py
@@ -12,7 +12,7 @@ IndexList = ['India%20VIX','Nifty%20100','Nifty%20200','Nifty%2050','Nifty%20500
              'Nifty%20EV','Nifty%20Mobility','Nifty%20Rural','Nifty%20Services','Nifty%20SME','Nifty%20TMT']
 
 ## list of trnx date to download in bulk
-TDate = ['2025-03-03']    
+TDate = ['2025-03-13']    
 
 for x in TDate:
     for i in IndexList:                
